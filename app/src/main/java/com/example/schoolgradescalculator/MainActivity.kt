42ef805746ec1a835d8b479375e31eab861129d1
Eliton Santos <elitonluiz1989@gmail.com>
2021-03-24 21:35:10 -0400

    Adding conditional result

--- app/src/main/java/com/example/schoolgradescalculator/MainActivity.kt
+++ app/src/main/java/com/example/schoolgradescalculator/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.schoolgradescalculator
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalculate.setOnClickListener {
+            val grade1 = Integer.parseInt(etGrade1.text.toString())
+            val grade2 = Integer.parseInt(etGrade2.text.toString())
+            val absences = Integer.parseInt(etAbsences.text.toString())
+            val average = (grade1 + grade2) / 2
+            val resultInfoText = """
+                Grades average: ${average.toString()}
+                Total absences: ${absences.toString()}
+            """.trimIndent()
+
+            if (average > 6 && absences <= 10) {
+                tvResult.text = "Approved"
+                tvResult.setTextColor(Color.WHITE)
+            } else {
+                tvResult.text = "Reproved."
+                tvResult.setTextColor(Color.RED)
+            }
+
+            tvResultInfo.text = resultInfoText
+        }
     }
 }
